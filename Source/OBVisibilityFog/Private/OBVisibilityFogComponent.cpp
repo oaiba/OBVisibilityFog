ae3397ac206f54a112e939cdc1282892fd61e528
Bá Oai <baoai25.work@gmail.com>
2025-09-22 17:35:19 +0700

    add OBVisibilityFogComponent implementation

---
+++ Source/OBVisibilityFog/Private/OBVisibilityFogComponent.cpp
@@ -0,0 +1,65 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OBVisibilityFogComponent.h"
+
+#include "Components/SceneCaptureComponent2D.h"
+
+UOBVisibilityFogComponent::UOBVisibilityFogComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UOBVisibilityFogComponent::InitializeFogComponents(USceneCaptureComponent2D* CaptureComponent,
+														UPostProcessComponent* PostProcessComponent)
+{
+	TargetSceneCapture = CaptureComponent;
+	TargetPostProcess = PostProcessComponent;
+
+	if (!TargetSceneCapture || !TargetPostProcess)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("InitializeFogComponents: One or more components are null. Disabling tick."));
+		SetComponentTickEnabled(false);
+	}
+}
+
+void UOBVisibilityFogComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (!IsValid(TargetSceneCapture) || !IsValid(TargetPostProcess))
+	{
+		UE_LOG(LogTemp, Warning,
+			   TEXT(
+				   "UOBVisibilityFogComponent on Actor '%s' is missing TargetSceneCapture or TargetPostProcess. Component will not tick."
+			   ), *GetOwner()->GetName());
+		SetComponentTickEnabled(false);
+		return;
+	}
+
+	if (const APawn* OwnerPawn = Cast<APawn>(GetOwner()); OwnerPawn && !OwnerPawn->IsLocallyControlled())
+	{
+		UE_LOG(LogTemp, Log, TEXT("UOBVisibilityFogComponent: Disabling for non-local pawn."));
+		SetComponentTickEnabled(false);
+		TargetSceneCapture->Deactivate();
+		TargetPostProcess->bEnabled = false;
+	}
+	else
+	{
+		TargetSceneCapture->bCaptureEveryFrame = false;
+		TargetSceneCapture->bCaptureOnMovement = false;
+		TargetPostProcess->bEnabled = true;
+		TargetSceneCapture->Activate();
+	}
+}
+
+void UOBVisibilityFogComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+											  FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (TargetSceneCapture && TargetSceneCapture->IsActive())
+	{
+		TargetSceneCapture->CaptureScene();
+	}
+}
---
+++ Source/OBVisibilityFog/Public/OBVisibilityFogComponent.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Components/PostProcessComponent.h"
+#include "OBVisibilityFogComponent.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class OBVISIBILITYFOG_API UOBVisibilityFogComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UOBVisibilityFogComponent();
+
+	/**
+	 * Function to set up required components from Blueprint or C++.
+	 * @param CaptureComponent SceneCapture2D component to be used.
+	 * @param PostProcessComponent PostProcess component to be used.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Visibility Fog")
+	void InitializeFogComponents(USceneCaptureComponent2D* CaptureComponent,
+								 UPostProcessComponent* PostProcessComponent);
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+							   FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visibility Fog|Dependencies")
+	USceneCaptureComponent2D* TargetSceneCapture = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visibility Fog|Dependencies")
+	UPostProcessComponent* TargetPostProcess = nullptr;
+
+protected:
+};
